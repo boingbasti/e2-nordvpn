a460b12c9362f17073020551f64360cd29b57726
boingbasti <117715317+boingbasti@users.noreply.github.com>
2026-05-05 09:08:02 +0200

    v1.3: Absturz behoben wenn /etc/openvpn/ beim Speichern der Zugangsdaten nicht existiert

--- tools/build_ipk.py
+++ tools/build_ipk.py
@@ -6,7 +6,7 @@ import tarfile
 import io
 
 PLUGIN_NAME   = "enigma2-plugin-extensions-nordvpn"
-VERSION       = "1.2"
+VERSION       = "1.3"
 ARCHITECTURE  = "all"
 MAINTAINER    = "boingbasti"
 DESCRIPTION   = "NordVPN OpenVPN-Client fuer Enigma2"
